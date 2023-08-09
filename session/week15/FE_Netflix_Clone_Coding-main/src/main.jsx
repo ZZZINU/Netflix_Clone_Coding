0c64ddc75c2ec1ae4c7a45961e7b9809b521743f
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-08-09 21:38:16 +0900

    feat: 재배포

--- session/week15/FE_Netflix_Clone_Coding-main/src/main.jsx
+++ session/week15/FE_Netflix_Clone_Coding-main/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="likelion11th">
+  <BrowserRouter basename="Netflix_Clone_Coding">
     <App />
   </BrowserRouter>
 );
--- session/week15/FE_Netflix_Clone_Coding-main/vite.config.js
+++ session/week15/FE_Netflix_Clone_Coding-main/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/likelion11th/",
+  base: "/Netflix_Clone_Coding/",
   plugins: [react()],
 });
